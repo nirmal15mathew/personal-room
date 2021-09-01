044193a3ef2ec16813d35109db633b7f332e0547
Nirmal Thomas Mathew <55346160+nirmal15mathew@users.noreply.github.com>
2021-09-01 12:45:42 +0530

    fixed mistake on line 16 used sensorState instead of sensorPin

--- main.ino
+++ main.ino
@@ -1,25 +1,29 @@
-#define sensorPin 19 // arbitrary value change later
-#define buzzerPin 3 // arbitrary value change later
-#define ledPin 8
+#define sensorPin 2 // arbitrary value change later
+#define buzzerPin 13 // arbitrary value change later
+#define ledPin 7
 bool sensorState = false;
 void setup() {
     pinMode(sensorPin, INPUT);
     pinMode(buzzerPin, OUTPUT);
     pinMode(ledPin, OUTPUT);
+  	Serial.begin(9600);
 }
 
 void loop() {
-    sensorState = digitalRead(sensorPin)
+    sensorState = digitalRead(sensorPin);
+  	Serial.println(sensorState);
 
-    if (sensorPin) {
-        digitalWrite(buzzerPin, HIGH)
-        delay(200)
-        digitalWrite(buzzerPin, LOW)
-        digitalWrite(ledPin, HIGH)
-        delay(4000)
+    if (sensorState == HIGH) {
+      	Serial.println("Sensed");
+        digitalWrite(buzzerPin, HIGH);
+        delay(200);
+        digitalWrite(buzzerPin, LOW);
+        digitalWrite(ledPin, HIGH);
+        delay(4000);
+      	digitalWrite(ledPin, LOW);
     }
     else {
-        digitalWrite(buzzerPin, LOW)
+        digitalWrite(buzzerPin, LOW);
     }
-    delay(100)
+    delay(100);
 }
